430127eef85eb39075ba28a59cfa673c8c22cca9
Uzziel Soto <uzziel.s627@stu.nebo.edu>
2021-11-18 09:22:04 -0700

    Finished pickups and worked on particels

--- Protoype 3 First Person Game/Assets/Scripts/Pickup.cs
+++ Protoype 3 First Person Game/Assets/Scripts/Pickup.cs
@@ -12,16 +12,34 @@ public class Pickup : MonoBehaviour
 {
     public PickupType type;
     public int value;
+
+    [Header("Bobbing Anim")]
+    public float rotationSpeed;
+    public float bobSpeed;
+    public float bobHeight;
+
+    private Vector3 startPos;
+    private bool bobbingUp;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Set the start position
+        startPos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Rotating
+        transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+
+        //Bob up and down
+        Vector3 offset = (bobbingUp == true ? new Vector3(0,bobHeight / 2, 0) : new Vector3(0, -bobHeight /2, 0));
+        transform.position = Vector3.MoveTowards(transform.position, startPos + offset, bobSpeed * Time.deltaTime);
+
+        if(transform.position == startPos + offset)
+            bobbingUp = !bobbingUp;
         
     }
 
