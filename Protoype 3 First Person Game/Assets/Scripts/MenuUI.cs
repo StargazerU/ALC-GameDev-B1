c8413bf829c0dd127c97cce82e5bdcb6db8de965
Uzziel Soto <uzziel.s627@stu.nebo.edu>
2022-01-10 09:11:45 -0700

    Worked on menu

---
+++ Protoype 3 First Person Game/Assets/Scripts/MenuUI.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MenuUI : MonoBehaviour
+{
+    public void OnPlayButton()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void OnQuitButton()
+    {
+        Application.Quit();
+    }
+}
